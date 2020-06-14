773bb5a2a97df4640dae412f7557a982538fc46f
Keesun Baik <whiteship2000@gmail.com>
2020-06-14 15:21:09 -0700

    functional interface and lambda

---
+++ src/main/java/me/whiteship/java8to11/Foo.java
@@ -0,0 +1,11 @@
+package me.whiteship.java8to11;
+
+public class Foo {
+
+    public static void main(String[] args) {
+        int baseNumber = 10;
+        RunSomething runSomething = number -> number + baseNumber;
+
+
+    }
+}
---
+++ src/main/java/me/whiteship/java8to11/RunSomething.java
@@ -0,0 +1,8 @@
+package me.whiteship.java8to11;
+
+@FunctionalInterface
+public interface RunSomething {
+
+    int doIt(int number);
+
+}
