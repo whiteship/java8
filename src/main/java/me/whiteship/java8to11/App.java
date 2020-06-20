9ea2572b551dcacd366532eaf324b70bfac27e4d
Keesun Baik <whiteship2000@gmail.com>
2020-06-19 21:12:49 -0700

    Stream

--- src/main/java/me/whiteship/java8to11/App.java
+++ src/main/java/me/whiteship/java8to11/App.java
@@ -2,17 +2,22 @@ package me.whiteship.java8to11;
 
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class App {
 
     public static void main(String[] args) {
-        List<String> name = new ArrayList<>();
-        name.add("keesun");
-        name.add("whiteship");
-        name.add("toby");
-        name.add("foo");
+        List<String> names = new ArrayList<>();
+        names.add("keesun");
+        names.add("whiteship");
+        names.add("toby");
+        names.add("foo");
 
-        name.forEach(System.out::println);
+        List<String> collect = names.stream().map((s) -> {
+            System.out.println(s + " " + Thread.currentThread().getName());
+            return s.toUpperCase();
+        }).collect(Collectors.toList());
+        collect.forEach(System.out::println);
     }
 
 }
