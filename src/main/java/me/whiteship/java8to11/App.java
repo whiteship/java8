f655965027ad6a66e0312f8818321aee23a795c1
Keesun Baik <whiteship2000@gmail.com>
2020-06-21 15:46:39 -0700

    Date Time

--- src/main/java/me/whiteship/java8to11/App.java
+++ src/main/java/me/whiteship/java8to11/App.java
@@ -1,30 +1,19 @@
 package me.whiteship.java8to11;
 
-import java.time.Duration;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Optional;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class App {
 
-    public static void main(String[] args) {
-        List<OnlineClass> springClasses = new ArrayList<>();
-        springClasses.add(new OnlineClass(1, "spring boot", true));
-        springClasses.add(new OnlineClass(5, "rest api development", false));
-
-        Optional<OnlineClass> optional = springClasses.stream()
-                .filter(oc -> oc.getTitle().startsWith("spring"))
-                .findFirst();
+    public static void main(String[] args) throws InterruptedException {
+        Calendar keesunBirthDay = new GregorianCalendar(1982, Calendar.JULY, 15);
+        System.out.println(keesunBirthDay.getTime());
+        keesunBirthDay.add(Calendar.DAY_OF_YEAR, 1);
+        System.out.println(keesunBirthDay.getTime());
     }
 
-    private static OnlineClass createNewClasss() {
-        System.out.println("creating new online class");
-        return new OnlineClass(10, "New class", false);
-    }
+
 
 
 }
