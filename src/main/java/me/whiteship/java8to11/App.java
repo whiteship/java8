fd837d5dc9f5855d98ea05ac660dfc12caf53567
Keesun Baik <whiteship2000@gmail.com>
2020-06-21 23:13:03 -0700

    Date Time API

--- src/main/java/me/whiteship/java8to11/App.java
+++ src/main/java/me/whiteship/java8to11/App.java
@@ -1,19 +1,35 @@
 package me.whiteship.java8to11;
 
-import java.util.Calendar;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.TimeZone;
 
 public class App {
 
-    public static void main(String[] args) throws InterruptedException {
-        Calendar keesunBirthDay = new GregorianCalendar(1982, Calendar.JULY, 15);
-        System.out.println(keesunBirthDay.getTime());
-        keesunBirthDay.add(Calendar.DAY_OF_YEAR, 1);
-        System.out.println(keesunBirthDay.getTime());
+    public static void main(String[] args) {
+        Date date = new Date();
+        Instant instant = date.toInstant();
+        Date newDate = Date.from(instant);
+
+        GregorianCalendar gregorianCalendar = new GregorianCalendar();
+        ZonedDateTime dateTime = gregorianCalendar.toInstant().atZone(ZoneId.systemDefault());
+        GregorianCalendar from = GregorianCalendar.from(dateTime);
+
+        ZoneId zoneId = TimeZone.getTimeZone("PST").toZoneId();
+        TimeZone timeZone = TimeZone.getTimeZone(zoneId);
+
+        LocalDateTime now = LocalDateTime.now();
+        now.plus(10, ChronoUnit.DAYS);
+
+        DateTimeFormatter MMddyyyy = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        System.out.println(now.format(MMddyyyy));
+
+        LocalDate parse = LocalDate.parse("07/15/1982", MMddyyyy);
+        System.out.println(parse);
     }
-
-
-
 
 }
