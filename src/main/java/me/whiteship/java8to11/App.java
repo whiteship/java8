81f7d04d4322c0d3dccbf1a2bf4a1840c41e41a8
Keesun Baik <whiteship2000@gmail.com>
2020-06-22 21:41:13 -0700

    Thread and Runnable

--- src/main/java/me/whiteship/java8to11/App.java
+++ src/main/java/me/whiteship/java8to11/App.java
@@ -1,35 +1,24 @@
 package me.whiteship.java8to11;
 
-import java.time.*;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
-import java.time.temporal.TemporalUnit;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.TimeZone;
-
 public class App {
 
     public static void main(String[] args) {
-        Date date = new Date();
-        Instant instant = date.toInstant();
-        Date newDate = Date.from(instant);
+        Thread thread = new Thread(() -> {
+            System.out.println("Thread: " + Thread.currentThread().getName());
+            try {
+                Thread.sleep(3000L);
+            } catch (InterruptedException e) {
+                throw new IllegalStateException(e);
+            }
+        });
+        thread.start();
 
-        GregorianCalendar gregorianCalendar = new GregorianCalendar();
-        ZonedDateTime dateTime = gregorianCalendar.toInstant().atZone(ZoneId.systemDefault());
-        GregorianCalendar from = GregorianCalendar.from(dateTime);
-
-        ZoneId zoneId = TimeZone.getTimeZone("PST").toZoneId();
-        TimeZone timeZone = TimeZone.getTimeZone(zoneId);
-
-        LocalDateTime now = LocalDateTime.now();
-        now.plus(10, ChronoUnit.DAYS);
-
-        DateTimeFormatter MMddyyyy = DateTimeFormatter.ofPattern("MM/dd/yyyy");
-        System.out.println(now.format(MMddyyyy));
-
-        LocalDate parse = LocalDate.parse("07/15/1982", MMddyyyy);
-        System.out.println(parse);
+        System.out.println("Hello: " + Thread.currentThread().getName());
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(thread + " is finished");
     }
-
 }
