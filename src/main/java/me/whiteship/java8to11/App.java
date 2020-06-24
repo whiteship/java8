fafa702a4899cd2e0f0b8de47bcb42f9961baf31
Keesun Baik <whiteship2000@gmail.com>
2020-06-23 22:21:05 -0700

    CompletableFuture

--- src/main/java/me/whiteship/java8to11/App.java
+++ src/main/java/me/whiteship/java8to11/App.java
@@ -1,33 +1,29 @@
 package me.whiteship.java8to11;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.concurrent.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 public class App {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        ExecutorService executorService = Executors.newFixedThreadPool(4);
+        boolean throwError = true;
 
-        Callable<String> hello = () -> {
-            Thread.sleep(2000L);
+        CompletableFuture<String> hello = CompletableFuture.supplyAsync(() -> {
+            if (throwError) {
+                throw new IllegalArgumentException();
+            }
+
+            System.out.println("Hello " + Thread.currentThread().getName());
             return "Hello";
-        };
+        }).handle((result, ex) -> {
+            if (ex != null) {
+                System.out.println(ex);
+                return "ERROR!";
+            }
+            return result;
+        });
 
-        Callable<String> java = () -> {
-            Thread.sleep(3000L);
-            return "Java";
-        };
-
-        Callable<String> keesun = () -> {
-            Thread.sleep(1000L);
-            return "Keesun";
-        };
-
-        String s = executorService.invokeAny(Arrays.asList(hello, java, keesun));
-        System.out.println(s);
-
-        executorService.shutdown();
+        System.out.println(hello.get());
     }
 
 }
